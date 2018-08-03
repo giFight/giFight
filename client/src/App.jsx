0801526122dec25b2b9abbf189c0d7cf8ad646ae
Matthew <mattsjsu14@gmail.com>
2018-08-03 16:45:42 -0700

    made 404, linked nav, btn now work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Home from './pages/Home';
-
-import Search from './pages/Search';
 import HowTo from './pages/HowTo';
 import AboutUs from './pages/AboutUs';
 import Convo from './pages/Convo';
+import NotFound from './pages/NotFound';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
@@ -17,7 +16,7 @@ const App = () => (
         <Route exact path ='/howto' component={HowTo}/>
         <Route exact path = '/aboutus' component={AboutUs}/>
         <Route exact path='/convo/:id' component={Convo} />
-
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
