fa308817aabb92c768eae368982ac85c16c31244
Matthew <mattsjsu14@gmail.com>
2018-08-03 16:53:21 -0700

    changed text

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -14,7 +14,7 @@ class AboutUs extends Component {
       <div className='home-main-container col-lg-8 mx-auto'>
         <Navbar />
         <Jumbotron>
-          <h1 className="display-4">Page Not Found!</h1>
+          <h1 className="display-4">PAGE NOT FOUND!</h1>
         </Jumbotron>
         <NotFoundGif />
       </div>
