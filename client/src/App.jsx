aee1ac9cd93be27384ead061c1a48d3f460fcbc6
Matthew <mattsjsu14@gmail.com>
2019-02-10 11:25:24 -0800

    added signin and login routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import HowTo from './pages/HowTo';
 import AboutUs from './pages/AboutUs';
 import Convo from './pages/Convo';
 import NotFound from './pages/NotFound';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
@@ -11,6 +13,8 @@ const App = () => (
   <Router>
     <div>
       <Switch>
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
         <Route exact path='/' component={Home} />
         <Route exact path='/home' component={Home} />
         <Route exact path ='/howto' component={HowTo}/>
